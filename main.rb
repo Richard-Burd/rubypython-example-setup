4e6ac706eeb31b8d02157ee2ff3855148322a93c
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-08-28 15:10:16 -0700

    deleted superfluous notes

--- main.rb
+++ main.rb
@@ -4,10 +4,6 @@ require "rubypython"
 puts "1. Welcome to the RubyPython Interpreter Page."
 puts "\n"
 
-# WARNING: According to this guy here: 
-# https://stackoverflow.com/questions/18144087/using-the-rubypython-gem-in-ruby-on-rails-how-do-you-call-a-python-script-from
-# ...RubyPython has a habbit of crashing $ rails server (Ruby on Rails)
-
 # This is the RubyPython Ruby #start method that will start the Python interpreter.
 RubyPython.start 
 
